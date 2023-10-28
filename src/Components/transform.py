49cc56689f82685d777b2fbc13b771dc292a0a16
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-10-28 23:04:25 +0900

    트랜스폼

---
+++ src/Components/transform.py
@@ -0,0 +1,38 @@
+from src.Components.component import CComponent
+from src.struct.vector2 import Vec2
+import sys
+sys.setrecursionlimit(10000000)
+
+class CTransform(CComponent):
+    def __init__(self):
+        super().__init__()
+        self.parent = None
+        self.childs = []
+        self.m_pos = Vec2()
+        self.m_size = Vec2()
+        self.m_degree = 0
+        self.m_scale = 1
+
+        self.m_posOffset = Vec2()
+
+    def GetWorldPos(self,acc = Vec2()):
+        if self.parent is not None:
+            return self.parent.GetWorldPos(self.m_pos + acc)
+        else:
+            return self.m_pos + acc
+    def GetWorldRotation(self,acc = 0):
+        if self.parent is not None:
+            return self.parent.GetWorldRotation(self.m_degree + acc)
+        else:
+            return self.m_degree + acc
+    def GetWolrdScale(self,acc = 1):
+        if self.parent is not None:
+            return self.parent.GetWorldScale(self.m_scale * acc)
+        else:
+            return self.m_scale * acc
+    def AddChild(self,transform):
+        transform.parent = self
+        self.childs.append(transform)
+    def FinalUpdate(self):
+        self.m_pos = self.m_posOffset
+        self.m_posOffset = Vec2()
---
+++ src/struct/vector2.py
@@ -0,0 +1,52 @@
+class Vec2:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __add__(self, other):
+        if isinstance(other, Vec2):
+            return Vec2(self.x + other.x, self.y + other.y)
+        else:
+            return NotImplemented
+
+    def __sub__(self, other):
+        if isinstance(other, Vec2):
+            return Vec2(self.x - other.x, self.y - other.y)
+        else:
+            return NotImplemented
+
+    def __mul__(self, scalar):
+        if isinstance(scalar, (int, float)):
+            return Vec2(self.x * scalar, self.y * scalar)
+        else:
+            return NotImplemented
+
+    def __truediv__(self, scalar):
+        if isinstance(scalar, (int, float)):
+            return Vec2(self.x / scalar, self.y / scalar)
+        else:
+            return NotImplemented
+
+    def length(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def normalize(self):
+        magnitude = self.length()
+        if magnitude > 0:
+            self.x /= magnitude
+            self.y /= magnitude
+
+    def normalized(self):
+        magnitude = self.length()
+        if magnitude > 0:
+            return Vec2(self.x / magnitude, self.y / magnitude)
+        return Vec2()
+
+def dot(a, b):
+    if isinstance(a, Vec2) and isinstance(b, Vec2):
+        return a.x * b.x + a.y * b.y
+    else:
+        raise ValueError("Arguments must be instances of Vec2.")
