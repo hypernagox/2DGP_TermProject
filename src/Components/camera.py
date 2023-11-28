dba40f42e0e9822c581db700280994b03e2132d7
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-11-28 23:20:00 +0900

    스크롤링메모

--- src/Components/camera.py
+++ src/Components/camera.py
@@ -8,7 +8,7 @@ class CCamera(CComponent):
     def __init__(self,obj):
         self.owner = obj
         self.m_transform = obj.GetTransform()
-        self.camOffset = Vec2(0,00)
+        self.camOffset = Vec2(0,-200)
     def world_to_screen(self, world_pos):
         width, height = CCore().GetWidthHeight()
         screen_x = world_pos.x - self.m_transform.m_pos.x + width / 2
