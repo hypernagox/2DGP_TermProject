5d0ee9569d910ffacc10bd004ce72f4d44fcc366
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-11-30 18:38:36 +0900

    대격변

--- src/Singletons/singleton.py
+++
@@ -1,7 +0,0 @@
-class SingletonBase(type):
-    g_instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls.g_instances:
-            instance = super().__call__(*args, **kwargs)
-            cls.g_instances[cls] = instance
-        return cls.g_instances[cls]
